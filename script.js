85f857a61244d580f15bf3cff7a58188f6f811bf
Hari prasad R <hariprasad3614@gmail.com>
2024-03-04 17:09:34 +0530

    first commit

--- script.js
+++ script.js
@@ -5,20 +5,22 @@ var messageError = document.getElementById('message-error');
 var submitError = document.getElementById('submit-error');
 
 function validateName(){
-  var name = document.getElementById('contact-name').value;
+  let name = document.getElementById("contact-name").value;
 
-  if(name.length == 0){
-    nameError.innerHTML = 'Name is required';
-    return false;
+  if (name.length == 0) {
+      nameError.innerHTML = "Name is required";
+      return false;
   }
-
   if(!name.match(/^([a-zA-Z ]){2,30}$/)){
-    nameError.innerHTML = 'Write name';
-    return false;
+      nameError.innerHTML = "Use only Aplhabets";
+      return false;
   }
-  nameError.innerHTML='<i class="bi bi-check-circle-fill"></i>';
-  return true;
+      nameError.innerHTML = '<i class="bi bi-check-circle-fill"></i>'
+      return true
+  
 }
+
+
 function validateEmail(){
   var email = document.getElementById('contact-email').value;
   if(email.length == 0){
